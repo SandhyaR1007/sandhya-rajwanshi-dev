533f449d7c6c5d0b2b10945f5345698130cd0b21
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-11-14 00:13:54 +0530

    add: fonts, hover effect on fonts

--- src/components/sidebarLeft/SidebarLeft.js
+++ src/components/sidebarLeft/SidebarLeft.js
@@ -4,10 +4,18 @@ const SidebarLeft = () => {
   return (
     <div className="sidebarLeft">
       <ul className="sidebarLeft__list">
-        <li>G</li>
-        <li>L</li>
-        <li>T</li>
-        <li>M</li>
+        <li className="sidebarLeft__list-items">
+          <i className="fa-brands fa-github-alt sidebarLeft__list-items__socials"></i>
+        </li>
+        <li className="sidebarLeft__list-items">
+          <i className="fa-brands fa-linkedin-in sidebarLeft__list-items__socials"></i>
+        </li>
+        <li className="sidebarLeft__list-items">
+          <i className="fa-brands fa-twitter sidebarLeft__list-items__socials"></i>
+        </li>
+        <li className="sidebarLeft__list-items">
+          <i className="fa-brands fa-medium sidebarLeft__list-items__socials"></i>
+        </li>
       </ul>
     </div>
   );
